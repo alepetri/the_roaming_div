eab84cae711b69963a1074c560c75b22509104a0
alepetri3 <alepetri3@gatech.edu>
2022-01-04 17:46:53 -0800

    Finished physics logic, roaming needs work

--- roaming_div.js
+++ roaming_div.js
@@ -2,7 +2,9 @@ class RoamingDiv {
     constructor(obj, container) {
         this.object = obj;
         this.container = container;
+        this.object.style.position = 'absolute';
         this.fps = 60;
+        this.base_speed = 100
 
         this.timestamp = null;
 
@@ -10,9 +12,12 @@ class RoamingDiv {
 
         const move_space = this.getMovableDimensions();
 
+        this.heading = Math.PI;
+        this.speed = this.base_speed;
+
         this.position = [Math.floor(move_space[0]/2), Math.floor(move_space[1]/2)]; // pixel (top-left of div relative to top-left of container)
         this.drawPosition();
-        this.velocity = [30, 30]; // pixel/sec
+        this.setSpeed(this.base_speed); // pixel/sec
         this.accelaration = [0, 0]; // pixel/sec^2
     }
 
@@ -41,26 +46,91 @@ class RoamingDiv {
             this.timestamp = now;
 
             const move_space = this.getMovableDimensions();
-            const dim0_inbounds = (this.position[0] > this.accel_padding[0]) && (this.position[0] < move_space[0] - this.accel_padding[0]);
-            const dim1_inbounds = (this.position[1] > this.accel_padding[1]) && (this.position[1] < move_space[1] - this.accel_padding[1]);
+            const dim0_inbounds = (this.position[0] >= this.accel_padding[0]) && (this.position[0] < move_space[0] - this.accel_padding[0]);
+            const dim1_inbounds = (this.position[1] >= this.accel_padding[1]) && (this.position[1] < move_space[1] - this.accel_padding[1]);
             if (dim0_inbounds && dim1_inbounds) {
-                this.propogate();
+                this.accelaration = [0,0];
+                if (this.speed !== this.base_speed) {
+                    this.setSpeed(this.base_speed);
+                }
+
+                // Add heading noise between -1 and 1 deg
+                this.setHeading(this.heading + (Math.random()-0.5)*6*Math.PI/180);
+            }
+            else {
+                if (!dim0_inbounds) {
+                    if (!this.accelaration[0]) {
+                        if (this.position[0] < this.accel_padding[0]) {
+                            this.accelaration[0] = (this.velocity[0]**2)/(2*(this.accel_padding[0]-1));
+                        }
+                        else {
+                            this.accelaration[0] = -(this.velocity[0]**2)/(2*(this.accel_padding[0]-1));
+                        }
+                    }
+                }
+                else {
+                    this.accelaration[0] = 0;
+                }
+                if (!dim1_inbounds) {
+                    if (!this.accelaration[1]) {
+                        if (this.position[1] < this.accel_padding[1]) {
+                            this.accelaration[1] = (this.velocity[1]**2)/(2*(this.accel_padding[1]-1));
+                        }
+                        else {
+                            this.accelaration[1] = -(this.velocity[1]**2)/(2*(this.accel_padding[1]-1));
+                        }
+                    }
+                }
+                else {
+                    this.accelaration[1] = 0;
+                }
             }
             
+            this.propogate();
             this.drawPosition();
         }
 
         window.requestAnimationFrame(this.getNextFrame.bind(this));
     }
 
+    setSpeed(speed) {
+        this.speed = speed;
+        this.updateVelocity();
+    }
+
+    setHeading(heading) {
+        this.heading = heading;
+        this.updateVelocity();
+    }
+
+    updateVelocity() {
+        this.velocity = [
+            this.speed * Math.sin(this.heading),
+            this.speed * Math.cos(this.heading)
+        ];
+    }
+
+    getHeading() {
+        return Math.atan2(this.velocity[0],this.velocity[1]);
+    }
+
     propogate() {
         const dt = 1/this.fps;
+
+        // if acceleration is non-zero, update velocity
+        if (this.accelaration.some(i => i)) {
+            this.velocity[0] += this.accelaration[0] * dt;
+            this.velocity[1] += this.accelaration[1] * dt;
+            this.heading = Math.atan2(this.velocity[0], this.velocity[1]);
+            this.speed = (this.velocity[0]**2 + this.velocity[1]**2)**0.5;
+        }
+
         this.position[0] += this.velocity[0] * dt;
         this.position[1] += this.velocity[1] * dt;
     }
 
     drawPosition() {
-        this.object.style.left = this.position[0] + 'px';
-        this.object.style.top = this.position[1] + 'px';
+        this.object.style.top = this.position[0] + 'px';
+        this.object.style.left = this.position[1] + 'px';
     }
 }
